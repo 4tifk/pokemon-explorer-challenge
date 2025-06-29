1987ad53c476a604e20c7cf4b188c4904df93cac
4tifk <katif6064@gmail.com>
2025-06-29 14:44:30 +0100

    Minor changes for consistency and update todo list

--- pokemon-explorer-challenge/backend/routes/pokemon.js
+++ pokemon-explorer-challenge/backend/routes/pokemon.js
@@ -10,15 +10,12 @@ const router = express.Router();
  * containing only the name, sprite, and types.
  *
  * TODO: Implement this route handler to:
- * 1. Fetch Pokemon data from https://pokeapi.co/api/v2/pokemon/{name}
- * 2. Extract only the name, sprite (front_default), and types
- * 3. Return a simplified JSON response
- * 4. Handle errors appropriately (404 for Pokemon not found, 500 for server errors)
+ * 1. Check if errors have been appropriately handled
  */
 router.get("/:name", async (req, res) => {
 
   const { name } = req.params;
-  const url = "https://pokeapi.co/api/v2/pokemon/" + name.toLowerCase();
+  const url = "https://pokeapi.co/api/v2/pokemon/" + name;
   let data;
   
   // fetch data
@@ -26,12 +23,12 @@ router.get("/:name", async (req, res) => {
     const response = await fetch (url);
 
     // return error if pokemon not found
-    if (response.status === 404){
+    if (response.status === 404) {
       return res.status(404).json({error: "Pokemon not found"});
     }
 
     // return error if server error
-    if (response.status === 500){
+    if (response.status === 500) {
       return res.status(500).json({error: "Server Error"});
     }
 
